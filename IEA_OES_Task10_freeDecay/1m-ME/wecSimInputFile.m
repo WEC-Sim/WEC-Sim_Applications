d1ec0a8a60d3b3a10aebf8bad77e6320b8eaa926
Kelley Ruehl <kmruehl@sandia.gov>
2019-07-19 13:17:12 -0600

    updated morison inputfile

--- IEA_OES_Task10_freeDecay/1m-ME/wecSimInputFile.m
+++ IEA_OES_Task10_freeDecay/1m-ME/wecSimInputFile.m
@@ -7,7 +7,7 @@ simu.CITime = 15;
 simu.explorer = 'off';
 simu.adjMassWeightFun = 0.47761120825;
 % Turn on Morison Element
-simu.morrisonElement    = 1; 
+simu.morisonElement    = 1; 
 
 %% Wave Information  
 % Regular Waves  
@@ -22,11 +22,11 @@ body(1).momOfInertia = [20907301 21306090.66 37085481.11];
 body(1).initDisp.initLinDisp = [ 0 0 1];    % Initial Displacement [m]
 
 % Morison Element Implementation
-body(1).morrisonElement.cd = [0 0 1];
-body(1).morrisonElement.ca = [0 0 1];
-body(1).morrisonElement.characteristicArea = [0 0 1];
-body(1).morrisonElement.VME = [0.01];
-body(1).morrisonElement.rgME = [0 0 -2];
+body(1).morisonElement.cd = [0 0 1];
+body(1).morisonElement.ca = [0 0 1];
+body(1).morisonElement.characteristicArea = [0 0 1];
+body(1).morisonElement.VME = [0.01];
+body(1).morisonElement.rgME = [0 0 -2];
 
 %% PTO and Constraint Parameters
 % Floating (3DOF) Joint
