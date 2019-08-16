66ace156751d573fd193903a921836887fe028b5
glennond <32114944+glennond@users.noreply.github.com>
2019-08-16 16:37:58 -0700

    almost done cleaning and commenting

--- NonlinearControls/FuzzyOTP/userDefinedFunctions.m
+++ NonlinearControls/FuzzyOTP/userDefinedFunctions.m
@@ -19,34 +19,34 @@
 % %Plot pitch moments for body 2
 % output.plotForces(2,5);
 
-simTime = logsout.getElement('heavePower').Values.Time;
-Position = logsout.getElement('heavePos').Values.Data;
-Power = logsout.getElement('heavePower').Values.Data;
-cumEnergy = cumsum(Power);
-totEnergy = trapz(simTime,Power)
-
-ax1 = subplot(3,1,1);
-plot(simTime, Position);
-ax1.XGrid = 'on';
-ylabel('Position');
-
-ax2 = subplot(3,1,2);
-plot(simTime, Power);
-ax2.XGrid = 'on';
-ylabel('Power');
-
-ax3 = subplot(3,1,3); 
-plot(simTime, cumEnergy);
-ax3.XGrid = 'on';
-ylabel('Energy');
-
-logsout.getElement('heaveVel').Values.Data;
-minVel = min(ans)
-maxVel = max(ans)
-Force = logsout.getElement('heaveFtot').Values.Data;
-minForce = min(Force)
-maxForce = max(Force)
-
-Position = logsout.getElement('heavePos').Values.Data;
-maxPos = max(Position)
-minPos = min(Position)
+% simTime = logsout.getElement('heavePower').Values.Time;
+% Position = logsout.getElement('heavePos').Values.Data;
+% Power = logsout.getElement('heavePower').Values.Data;
+% cumEnergy = cumsum(Power);
+% totEnergy = trapz(simTime,Power)
+% 
+% ax1 = subplot(3,1,1);
+% plot(simTime, Position);
+% ax1.XGrid = 'on';
+% ylabel('Position');
+% 
+% ax2 = subplot(3,1,2);
+% plot(simTime, Power);
+% ax2.XGrid = 'on';
+% ylabel('Power');
+% 
+% ax3 = subplot(3,1,3); 
+% plot(simTime, cumEnergy);
+% ax3.XGrid = 'on';
+% ylabel('Energy');
+% 
+% logsout.getElement('heaveVel').Values.Data;
+% minVel = min(ans)
+% maxVel = max(ans)
+% Force = logsout.getElement('heaveFtot').Values.Data;
+% minForce = min(Force)
+% maxForce = max(Force)
+% 
+% Position = logsout.getElement('heavePos').Values.Data;
+% maxPos = max(Position)
+% minPos = min(Position)
