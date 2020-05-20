7d0c922929e37cac1ac9261f0e0d659a85683e61
Kelley Ruehl <kmruehl@sandia.gov>
2020-05-20 13:30:22 -0600

    input file cleanup

--- NonlinearHydro/ode4/Regular/wecSimInputFile.m
+++ NonlinearHydro/ode4/Regular/wecSimInputFile.m
@@ -1,38 +1,37 @@
 %% Simulation Data
-simu = simulationClass();               
-simu.simMechanicsFile = 'ellipsoid.slx';   
-simu.mode = 'normal';                   % Specify Simulation Mode ('normal','accelerator','rapid-accelerator')
-simu.explorer='on';                     % Turn SimMechanics Explorer (on/off)
-simu.startTime = 0;                     
-simu.rampTime = 50;                        
-simu.endTime=150;                       
-simu.dt = 0.05;                         
-simu.rho=1025;                                                                                                           
-simu.nlHydro = 2;                       % Non-linear hydro on/off
+simu = simulationClass();                       % Initialize simulationClass        
+simu.simMechanicsFile = 'ellipsoid.slx';        % Simulink Model File   
+simu.mode = 'normal';                           % Simulation Mode 
+simu.explorer='on';                             % Turn SimMechanics Explorer (on/off)
+simu.startTime = 0;                             % Simulation Start Time [s]
+simu.rampTime = 50;                             % Simulation Ramp Time [s]    
+simu.endTime=150;                               % Simulation End Time [s]
+simu.dt = 0.05;                                 % Simulation time-step [s]
+simu.rho=1025;                                  % Specify water desnity [kg/m^3]                                                                                   
+simu.nlHydro = 2;                               % Non-linear hydro on/off
 
 %% Wave Information
 % Regular Waves 
-waves = waveClass('regular');                 
-waves.H = 4;                            
-waves.T = 6;   
+waves = waveClass('regular');                   % Initialize waveClass        
+waves.H = 4;                                    % Wave Height [m]
+waves.T = 6;                                    % Wave Period [s]
 
 %% Body Data
-body(1) = bodyClass('../../hydroData/ellipsoid.h5');
-body(1).mass = 'equilibrium';           
-body(1).momOfInertia = ...              
+body(1) = bodyClass('../../hydroData/ellipsoid.h5');    % Initialize bodyClass
+body(1).geometryFile = '../../geometry/elipsoid.stl';   % Discretized body geometry for nlHydro
+body(1).mass = 'equilibrium';                           % Specify Mass [kg]
+body(1).momOfInertia = ...                              % Specify MOI
     [1.375264e6 1.375264e6 1.341721e6];      
-body(1).geometryFile = '../../geometry/elipsoid.stl' ;    
-body(1).viscDrag.cd=[1 0 1 0 1 0];
+body(1).viscDrag.cd=[1 0 1 0 1 0];  
 body(1).viscDrag.characteristicArea=[25 0 pi*5^2 0 pi*5^5 0];
 
 %% PTO and Constraint Parameters
 % Fixed Constraint
-constraint(1) = constraintClass('Constraint1'); 
-constraint(1).loc = [0 0 -12.5];        
+constraint(1) = constraintClass('Constraint1'); % Initialize constraintClass
+constraint(1).loc = [0 0 -12.5];                % Constraint Location [m]
 
 % Translational PTO
-pto(1) = ptoClass('PTO1');              
-pto(1).k=0;                             
-pto(1).c=1200000;                      
-pto(1).loc = [0 0 -12.5];
-pto(1).loc = [0 0 -12.5];
+pto(1) = ptoClass('PTO1');                      % Initialize ptoClass
+pto(1).k=0;                                     % PTO Stiffness Coeff [N/m]
+pto(1).c=1200000;                               % PTO Damping Coeff [Ns/m]
+pto(1).loc = [0 0 -12.5];                       % PTO Location [m]
