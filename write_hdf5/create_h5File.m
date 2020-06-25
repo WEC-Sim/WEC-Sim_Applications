cf49de6a460b383ee727d67ec2c0f95d55864265
Kelley Ruehl <kmruehl@sandia.gov>
2020-06-24 18:44:55 -0600

    updating write_hdf5 hydrodata for rm3 example

--- write_hdf5/create_h5File.m
+++ write_hdf5/create_h5File.m
@@ -1,7 +1,6 @@
 
-
 % create or load data
-load('hydroData.mat');
+load('rm3_hydroData.mat')
 
 % create h5 file and write string datasets
 h5bem_create('rm3.h5', nBody, nFreq, nDir, bodyNames, scaled);
@@ -10,5 +9,5 @@ h5bem_create('rm3.h5', nBody, nFreq, nDir, bodyNames, scaled);
 h5bem_writeParameters('rm3.h5', wave_dir, depth, w, T);
 
 % write each body
-h5bem_writeBody('rm3.h5', 1, b1_cg, b1_dispVol, b1_k, b1_reEx, b1_imEx, b1_am, b1_amInf, b1_rd); 
-h5bem_writeBody('rm3.h5', 2, b2_cg, b2_dispVol, b2_k, b2_reEx, b2_imEx, b2_am, b2_amInf, b2_rd); 
+h5bem_writeBody('rm3.h5', 1, b1_cb, b1_cg, b1_dispVol, b1_k, b1_reEx, b1_imEx, b1_am, b1_amInf, b1_rd); 
+h5bem_writeBody('rm3.h5', 2, b2_cb, b2_cg, b2_dispVol, b2_k, b2_reEx, b2_imEx, b2_am, b2_amInf, b2_rd); 
