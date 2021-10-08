a64c3793547758e53401bfdf77ac906fc6f9b9cd
kmruehl <kmruehl@sandia.gov>
2021-10-08 15:01:33 -0600

    cleanup End_Stops

--- End_Stops/wecSimInputFile.m
+++ End_Stops/wecSimInputFile.m
@@ -6,7 +6,7 @@ simu.explorer='off';
 simu.startTime = 0;                     
 simu.rampTime = 100;                       
 simu.endTime=400;                       
-simu.dt = 0.01; 							
+simu.dt = 0.1; 							
 simu.b2b = 0;                   	% Turn B2B interactions 'off'                      
 
 %% Wave Information 
@@ -17,14 +17,14 @@ waves.T = 8;
 
 %% Body Data
 % Float
-body(1) = bodyClass('../hydroData/rm3.h5');      
-body(1).geometryFile = '../geometry/float.stl';   
+body(1) = bodyClass('./hydroData/rm3.h5');      
+body(1).geometryFile = './geometry/float.stl';   
 body(1).mass = 'equilibrium';                   
 body(1).momOfInertia = [20907301 21306090.66 37085481.11];       
 
 % Spar/Plate
-body(2) = bodyClass('../hydroData/rm3.h5'); 
-body(2).geometryFile = '../geometry/plate.stl'; 
+body(2) = bodyClass('./hydroData/rm3.h5'); 
+body(2).geometryFile = './geometry/plate.stl'; 
 body(2).mass = 'equilibrium';                   
 body(2).momOfInertia = [94419614.57 94407091.24 28542224.82];
 
@@ -40,16 +40,18 @@ pto(1).c=1200000;
 pto(1).loc = [0 0 0];                    	
 
 % End Stop specification
-% These are parameters used by the custom matlab function describing the
-% actuation force
-upperLimitBound = 0.6; % upper limit at +0.8 m
-lowerLimitBound = -0.6; % lower limit at -0.8 m 
-upperLimitStiffness = 1e8; % upper limit spring stiffness N/m
-lowerLimitStiffness = 1e8; % lower limit spring stiffness N/m
-upperLimitDamping = 1e3; % % upper limit damping N/m/s
-lowerLimitDamping = 1e3; % lower limit damping N/m/s 
-upperTransitionRegionWidth = 0.1; % upper transition region width, m
-lowerTransitionRegionWidth = 0.1;  % lower transition region width, m
+pto(1).hardStops.upperLimitSpecify = 'on'; % enable upper limit
+pto(1).hardStops.lowerLimitSpecify = 'on'; % enable lower limit
+pto(1).hardStops.upperLimitBound = 0.6; % upper limit at +0.8 m
+pto(1).hardStops.lowerLimitBound = -0.6; % lower limit at -0.8 m 
+pto(1).hardStops.upperLimitStiffness = 1e8; % upper limit at +0.8 m
+pto(1).hardStops.lowerLimitStiffness = 1e8; % lower limit at -0.8 m 
+pto(1).hardStops.upperLimitDamping = 0; % upper limit at +0.8 m
+pto(1).hardStops.lowerLimitDamping = 0; % lower limit at -0.8 m 
+pto(1).hardStops.upperLimitTransitionRegion = 0.5; % upper limit at +0.8 m
+pto(1).hardStops.lowerLimitTransitionRegion = 0.5; % lower limit at -0.8 m 
+
+
 
 
 
