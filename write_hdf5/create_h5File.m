bfeb720268f52b67c1c4605830078048448e1f62
akeeste <akeeste@sandia.gov>
2022-03-01 14:16:49 -0700

    update write hdf5 application

--- write_hdf5/create_h5File.m
+++ write_hdf5/create_h5File.m
@@ -1,13 +1,70 @@
 
-% create or load data
+% create or load data to write to the H5 file
 load('rm3_hydroData.mat')
 
-% create h5 file and write string datasets
-h5bem_create('rm3.h5', nBody, nFreq, nDir, bodyNames, scaled);
+% Assign variables to the hydro structure. See
+% https://wec-sim.github.io/WEC-Sim/master/user/advanced_features.html#bemio-hydro-data-structure
+% for the full layout.
+hydro = struct();
 
-% write simulation_parameters datasets
-h5bem_writeParameters('rm3.h5', wave_dir, depth, w, T);
+% Simulation properties
+hydro.file = 'experimental';
+hydro.code = '';
+hydro.g = 9.81;
+hydro.rho = 1000;
+hydro.body = bodyNames;
+hydro.h = depth;
+hydro.Nb = nBody;
+hydro.Nf = nFreq;
+hydro.Nh = nDir;
+hydro.T = T;
+hydro.w = w;
+hydro.beta = wave_dir;
 
-% write each body
-h5bem_writeBody('rm3.h5', 1, b1_cb, b1_cg, b1_dispVol, b1_k, b1_reEx, b1_imEx, b1_am, b1_amInf, b1_rd); 
-h5bem_writeBody('rm3.h5', 2, b2_cb, b2_cg, b2_dispVol, b2_k, b2_reEx, b2_imEx, b2_am, b2_amInf, b2_rd); 
+% Body Properties
+hydro.cg = zeros(3,nBody);
+hydro.cg(:,1) = b1_cg;
+hydro.cg(:,2) = b2_cg;
+
+hydro.cb = zeros(3,nBody);
+hydro.cb(:,1) = b1_cb;
+hydro.cb(:,2) = b2_cb;
+
+hydro.Vo = zeros(1,nBody);
+hydro.Vo(1,1) = b1_dispVol;
+hydro.Vo(1,2) = b2_dispVol;
+
+% Added mass
+hydro.A = zeros(6*nBody,6*nBody,nFreq);
+hydro.Ainf = zeros(6*nBody,6*nBody);
+hydro.A(1:6,:,:) = b1_am;
+hydro.A(7:12,:,:) = b2_am;
+hydro.Ainf(1:6,:,:) = b1_amInf;
+hydro.Ainf(7:12,:,:) = b2_amInf;
+
+% Damping
+hydro.B = zeros(6*nBody,6*nBody,nFreq);
+hydro.B(1:6,:,:) = b1_rd;
+hydro.B(7:12,:,:) = b2_rd;
+hydro = radiationIRF(hydro,[],[],[],[],[]);
+hydro = radiationIRFSS(hydro,[],[]);
+
+% Stiffness
+hydro.C = zeros(6,6,nBody);
+hydro.C(:,:,1) = b1_k;
+hydro.C(:,:,2) = b2_k;
+
+% Excitation force
+hydro.ex_re = zeros(6*nBody,nDir,nFreq);
+hydro.ex_im = zeros(6*nBody,nDir,nFreq);
+hydro.ex_re(1:6,:,:) = b1_reEx;
+hydro.ex_re(7:12,:,:) = b2_reEx;
+hydro.ex_im(1:6,:,:) = b1_imEx;
+hydro.ex_im(7:12,:,:) = b2_imEx;
+
+% Calculate excitation phase, magnitude and IRF
+hydro.ex_ma = (hydro.ex_re.^2 + hydro.ex_im.^2).^0.5;
+hydro.ex_ph = angle(hydro.ex_re + 1i*hydro.ex_im);
+hydro = excitationIRF(hydro,[],[],[],[],[]);
+
+writeBEMIOH5(hydro);
