d8928323fa7385c38f1569ba6ae1433e56c29d24
kmruehl <kmruehl@sandia.gov>
2022-03-08 14:24:36 -0700

    updating passive yaw to bodyClass

--- Passive_Yaw/PassiveYawON/wecSimInputFile.m
+++ Passive_Yaw/PassiveYawON/wecSimInputFile.m
@@ -8,30 +8,31 @@ simu.rampTime = 100;                    % Wave Ramp Time [s]
 simu.endTime=600;                       % Simulation End Time [s]        
 simu.solver = 'ode4';                   % simu.solver = 'ode4' for fixed step & simu.solver = 'ode45' for variable step 
 simu.dt = 0.01;                         % Simulation Time-Step [s]
-simu.cicEndTime = 40;                       % Specify CI Time [s]
-simu.yawNonLin=1;                       % Turn passive yaw ON
-simu.yawThresh=0.01;                    % Set passive yaw threshold
+simu.cicEndTime = 40;                   % Specify CI Time [s]
 
 %% Wave Information
 % % Regular Waves 
 waves = waveClass('regular');           % Initialize Wave Class and Specify Type                                 
 waves.H = 2.5;                          % Wave Height [m]
 waves.T = 8;                            % Wave Period [s]
-waves.waveDirection = [10];                   % Wave Directionality [deg]
-waves.waveSpread = [1];                 % Wave Directional Spreading [%}
+waves.direction = 10;                   % Wave Directionality [deg]
+waves.spread = 1;                       % Wave Directional Spreading [%}
 
 %% Body Data
 % Flap NOTE: This test uses unique BEM for the OSWEC
-body(1) = bodyClass('../hydroData/oswec.h5');      % Initialize bodyClass for Flap
-body(1).geometryFile = '../geometry/flap.stl';     % Geometry File
+body(1) = bodyClass('../hydroData/oswec.h5');   % Initialize bodyClass for Flap
+body(1).geometryFile = '../geometry/flap.stl';  % Geometry File
 body(1).mass = 12700;                           % User-Defined mass [kg]
 body(1).momOfInertia = [1.85e6 1.85e6 1.85e6];  % Moment of Inertia [kg-m^2]
-%body(1).viscDrag.cd = [3 3 3 3 3 3];
+body(1).yaw.option=1;                           % Turn passive yaw ON
+body(1).yaw.threshold=0.01;                     % Set passive yaw threshold
 
 % Base NOTE: This test uses unique BEM for the OSWEC
-body(2) = bodyClass('../hydroData/oswec.h5');      % Initialize bodyClass for Base
-body(2).geometryFile = '../geometry/base.stl';     % Geometry File
+body(2) = bodyClass('../hydroData/oswec.h5');   % Initialize bodyClass for Base
+body(2).geometryFile = '../geometry/base.stl';  % Geometry File
 body(2).mass = 'fixed';                         % Creates Fixed Body
+body(2).yaw.option=1;                           % Turn passive yaw ON
+body(2).yaw.threshold=0.01;                     % Set passive yaw threshold
 
 %% PTO and Constraint Parameters
 % Fixed
--- Passive_Yaw/PassiveYawRegression/wecSimInputFile.m
+++ Passive_Yaw/PassiveYawRegression/wecSimInputFile.m
@@ -9,17 +9,14 @@ simu.endTime=250;                       % Simulation End Time [s]
 simu.solver = 'ode4';                   % simu.solver = 'ode4' for fixed step & simu.solver = 'ode45' for variable step 
 simu.dt = 0.01;                         % Simulation Time-Step [s]
 simu.cicEndTime = 40;                       % Specify CI Time [s]
-%simu.dtCITime=0.001;
-simu.yawNonLin=1;
-simu.yawThresh=1;
 
 %% Wave Information
 % % Irregular Waves 
 waves = waveClass('irregular');         % Initialize Wave Class and Specify Type                                 
 waves.H = 2.5;                          % Wave Height [m]
 waves.T = 8;                            % Wave Period [s]
-waves.waveDirection = [10];                   % Wave Directionality [deg]
-waves.waveSpread = [1];                 % Wave Directional Spreading [%}
+waves.direction = 10;                   % Wave Directionality [deg]
+waves.spread = 1;                       % Wave Directional Spreading [%}
 waves.spectrumType='PM';                % Wave spectrum type
 waves.phaseSeed=1;                      % Specify phase so repeatable
 
@@ -29,12 +26,15 @@ body(1) = bodyClass('../hydroData/oswec.h5');      % Initialize bodyClass for Fl
 body(1).geometryFile = '../geometry/flap.stl';     % Geometry File
 body(1).mass = 12700;                           % User-Defined mass [kg]
 body(1).momOfInertia = [1.85e6 1.85e6 1.85e6];  % Moment of Inertia [kg-m^2]
-%body(1).viscDrag.cd = [3 3 3 3 3 3];
+body(1).yaw.option=1;                           % Turn passive yaw ON
+body(1).yaw.threshold=0.01;                     % Set passive yaw threshold
 
 % Base NOTE: This test uses unique BEM for the OSWEC
 body(2) = bodyClass('../hydroData/oswec.h5');      % Initialize bodyClass for Base
 body(2).geometryFile = '../geometry/base.stl';     % Geometry File
 body(2).mass = 'fixed';                         % Creates Fixed Body
+body(2).yaw.option=1;                           % Turn passive yaw ON
+body(2).yaw.threshold=0.01;                     % Set passive yaw threshold
 
 %% PTO and Constraint Parameters
 % Fixed
