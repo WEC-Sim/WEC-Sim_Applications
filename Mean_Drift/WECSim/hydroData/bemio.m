bc67bc78380b5802d6d6851542c5594dcb5be0e9
nathanmtom <nathan.tom@nrel.gov>
2022-03-15 16:18:37 -0600

    Addition of a mean drift application case for a sphere (#18)
    * @nathanmtom addition of of a mean drift application case to WEC-Sim Applications
    
    * updated mean drift for refactoring
    
    Co-authored-by: kmruehl <kmruehl@sandia.gov>

---
+++ Mean_Drift/WECSim/hydroData/bemio.m
@@ -0,0 +1,28 @@
+% clc; clear all; close all;
+
+%% hydro data
+hydro = struct();
+hydro = readWAMIT(hydro,'sphere.out',[]);
+hydro = radiationIRF(hydro,10,[],[],[],[]);
+hydro = radiationIRFSS(hydro,[],[]);
+hydro = excitationIRF(hydro,10,[],[],[],[]);
+writeBEMIOH5(hydro)
+
+%% Plot hydro data
+plotBEMIO(hydro)
+
+%% Plot the mean drift coefficients
+% Sphere radius is 1 m
+% The wave number in deep water is equal \omega^{2}/g
+kr      = (2*pi./(hydro.T)).^2/9.81*1;
+MC(1,:)=hydro.md_mc(1,1,:);
+PI(1,:)=hydro.md_pi(1,1,:);
+CS(1,:)=hydro.md_cs(1,1,:);
+%
+close all
+plot((2*pi./(hydro.T)).^2/9.81,MC,...
+     (2*pi./(hydro.T)).^2/9.81,PI,'--',...
+     (2*pi./(hydro.T)).^2/9.81,CS,'-.');grid on
+xlabel('Wave Number \times Sphere Radius, kr, [-]');xlim([0 3]);
+ylabel('Normalized Surge Mean Drift Force, f_{md,1}/\rhog, [1/m^{2}]');ylim([0 1]);
+legend('Momentum Conservation','Pressure Integration','Control Surfaces')
---
+++ Mean_Drift/WECSim/wecSimInputFile.m
@@ -0,0 +1,38 @@
+%% Simulation Data
+simu                    = simulationClass();               
+simu.simMechanicsFile   = 'sphere.slx';
+simu.rampTime           = 20;
+simu.endTime            = 100;                        
+simu.dt                 = 0.01;                         
+simu.cicEndTime         = 10;
+simu.explorer           = 'on';
+simu.domainSize         = 25;
+
+%% Wave Information  
+% Regular Waves  
+waves = waveClass('regularCIC');
+waves.T = 2;
+waves.H = 0.1;
+
+%% Body Data
+% Float
+body(1) = bodyClass('hydroData/sphere.h5');    	
+body(1).geometryFile = 'geometry/sphere.stl';      
+body(1).mass = 'equilibrium';
+sphereRadius    = 1;
+sphereVol       = 4/3*pi*sphereRadius^3;
+sphereMass      = (1/2)*simu.rho*sphereVol;
+Ixx             = (2/5)*sphereMass*sphereRadius^(2); Iyy = Ixx; Izz = Iyy;
+body(1).momOfInertia = [Ixx Iyy Izz];
+
+%% Mean Drift Option
+% (`integer`) Flag for mean drift force with three options:  
+%      0 (no), 
+%      1 (from control surface) 
+%      2 (from momentum conservation)
+body(1).meanDrift = 1;                                
+
+%% PTO and Constraint Parameters
+% Floating (3DOF) Joint
+constraint(1) = constraintClass('Constraint1'); 
+constraint(1).location = [0 0 0];                   
