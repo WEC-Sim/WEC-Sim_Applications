037947b1d8ca2cdc23d5f37723a9e69a791ebbf8
jtgrasb <jtgrasb@sandia.gov>
2023-09-26 15:54:31 -0400

    Fix bemio

--- MOST/hydroData/bemio.m
+++ MOST/hydroData/bemio.m
@@ -5,6 +5,7 @@ foldernemoh = 'VolturnUS15MW_nemoh';
 
 hydro = struct();
 hydro = readNEMOH(hydro,[pwd,filesep,foldernemoh,filesep]);
-hydro = Radiation_IRF(hydro,90,201,201,[],[]);
-hydro = Excitation_IRF(hydro,90,[],[],[],[]);
-Write_H5(hydro)
+hydro = radiationIRF(hydro,90,201,201,[],[]);
+hydro = radiationIRFSS(hydro,[],[]);
+hydro = excitationIRF(hydro,90,[],[],[],[]);
+writeBEMIOH5(hydro)
