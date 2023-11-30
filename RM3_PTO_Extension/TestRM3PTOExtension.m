713077055d7e16e945c1af1d258989698ed19d14
Allison-File <allison.file@nrel.gov>
2023-11-30 00:41:15 -0700

    Create TestRM3PTOExtension.m

---
+++ RM3_PTO_Extension/TestRM3PTOExtension.m
@@ -0,0 +1,64 @@
+classdef TestRM3PTOExtension < matlab.unittest.TestCase
+    
+    properties
+        OriginalDefault
+        testDir
+        h5Dir = "hydroData"
+        h5Name = 'rm3.h5'
+        outName = 'rm3.out'
+    end    
+    
+    methods (Access = 'public')        
+        function obj = TestRM3PTOExtension
+            obj.testDir = fileparts(mfilename('fullpath'));
+        end    
+    end
+    
+    methods (TestMethodSetup)
+        function killPlots (~)
+            set(0,'DefaultFigureVisible','off');
+        end
+    end
+    
+    methods(TestClassSetup)        
+        function captureVisibility(testCase)
+            testCase.OriginalDefault = get(0,'DefaultFigureVisible');
+        end        
+        function runBemio(testCase)            
+            cd(testCase.h5Dir);
+            if isfile(testCase.h5Name)
+                fprintf('runBemio skipped, *.h5 already exists\n')
+            else
+                bemio
+            end
+            cd(testCase.testDir)            
+        end        
+    end
+    
+    methods(TestMethodTeardown)
+        function returnHome(testCase)
+            cd(testCase.testDir)
+        end
+    end
+    
+    methods(TestClassTeardown)        
+        function checkVisibilityRestored(testCase)
+            set(0,'DefaultFigureVisible',testCase.OriginalDefault);
+            testCase.assertEqual(get(0,'DefaultFigureVisible'),     ...
+                                 testCase.OriginalDefault);
+        end
+    end
+    
+    methods(Test)        
+        function testRM3_PTO_Extension_float(testCase)
+            cd('float')
+            wecSim
+            cd(testCase.testDir)
+        end        
+        function testRM3_PTO_Extension_spar(testCase)
+            cd('spar')
+            wecSim
+            cd(testCase.testDir)
+        end              
+    end    
+end
