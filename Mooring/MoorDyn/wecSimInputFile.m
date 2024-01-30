f3a1e93fa981c377505afb0fe283f169af002d88
jtgrasb <jtgrasb@sandia.gov>
2024-01-30 10:52:08 -0700

    Clean up wecSimInputFile.m

--- Mooring/MoorDyn/wecSimInputFile.m
+++ Mooring/MoorDyn/wecSimInputFile.m
@@ -1,5 +1,5 @@
 %% Simulation Data
-simu = simulationClass();                  
+simu = simulationClass();             
 simu.simMechanicsFile = 'RM3MoorDyn.slx';       % WEC-Sim Model File
 simu.mode = 'accelerator';                
 simu.explorer = 'off';
@@ -25,12 +25,12 @@ body(2) = bodyClass('../hydroData/rm3.h5');
 body(2).geometryFile = '../geometry/plate.stl';
 body(2).mass = 'equilibrium';                   
 body(2).inertia = [94419614.57 94407091.24 28542224.82];
-body(2).initial.displacement = [0 0 -.21];      % Initial Displacement
+body(2).initial.displacement = [0 0 -0.21];     % Initial Displacement
 
 %% PTO and Constraint Parameters
 % Floating (3DOF) Joint
 constraint(1) = constraintClass('Constraint1'); 
-constraint(1).location = [0 0 0];      
+constraint(1).location = [0 0 0];       
 
 % Translational PTO
 pto(1) = ptoClass('PTO1');              	
@@ -40,9 +40,9 @@ pto(1).location = [0 0 0];
 
 %% Mooring
 % Moordyn
-mooring(1) = mooringClass('mooring1');          % Initialize mooringClass
+mooring(1) = mooringClass('mooring');          % Initialize mooringClass
 mooring(1).moorDyn = 1;                         % Initialize MoorDyn                                                                    
 mooring(1).moorDynLines = 6;                    % Specify number of lines
-mooring(1).moorDynNodes(1:3) = 6;               % Specify number of nodes per line
-mooring(1).moorDynNodes(4:6) = 16;              % Specify number of nodes per line
+mooring(1).moorDynNodes(1:3) = 16;               % Specify number of nodes per line
+mooring(1).moorDynNodes(4:6) = 6;              % Specify number of nodes per line
 mooring(1).initial.displacement = [0 0 -0.21];  % Initial Displacement
