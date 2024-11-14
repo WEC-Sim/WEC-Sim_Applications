a9ce564ee381d904dc9f89bf268ccf5f064381d1
Mathew Topper <damm_horse@yahoo.co.uk>
2024-11-14 09:50:30 +0000

    Use a test that actually works

--- Cable/TestCable.m
+++ Cable/TestCable.m
@@ -8,6 +8,7 @@ classdef TestCable < matlab.unittest.TestCase
         outName = 'mbari.out'
     end
     
+    
     methods (Access = 'public')
         function obj = TestCable
             obj.testDir = fileparts(mfilename('fullpath'));
@@ -43,7 +44,7 @@ classdef TestCable < matlab.unittest.TestCase
         function checkVisibilityRestored(testCase)
             set(0, 'DefaultFigureVisible', testCase.OriginalDefault);
             testCase.assertEqual(get(0, 'DefaultFigureVisible'),    ...
-                testCase.OriginalDefault);
+                                 testCase.OriginalDefault);
         end
         
     end
