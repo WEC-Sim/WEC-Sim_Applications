3d263039a3e11972ff22f5bd6a76fc93e0d80637
Shabara <mshabara@nrel.gov>
2024-11-28 19:02:13 -0700

    Sets the slx file version to 2020b

--- _Common_Input_Files/Floating_OWC/hydroData/bemio.m
+++ _Common_Input_Files/Floating_OWC/hydroData/bemio.m
@@ -1,6 +1,10 @@
+clc; clear all; close all;
+
 %% hydro data
 hydro = struct();
 hydro = readAQWA(hydro, 'ANALYSIS.AH1', 'ANALYSIS.LIS');
+hydro.cg(:,2) = [0;0;-2.5];
+hydro.cb(:,2) = [0;0;-2.5];
 hydro = radiationIRF(hydro,150,[],[],[],[]);
 hydro = radiationIRFSS(hydro,[],[]);
 hydro = excitationIRF(hydro,150,[],[],[],[]);
