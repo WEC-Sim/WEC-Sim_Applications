a474f27b227cc7af925ed61da914fc530dd2b5f5
jleonqu <jleonqu@sandia.gov>
2024-12-11 11:30:21 -0700

    Fixing the bemio.m file for the test17a.out WAMIT output

--- OWC/OrificeModel/OrificeModel/hydroData/bemio.m
+++ OWC/OrificeModel/OrificeModel/hydroData/bemio.m
@@ -4,6 +4,6 @@ hydro = readWAMIT(hydro,'test17a.out',[]);
 hydro = radiationIRF(hydro,20,[],[],[],11);
 hydro = radiationIRFSS(hydro,20,[]);
 hydro = excitationIRF(hydro,20,[],[],[],11);
-hydro.plotDofs = [1,1;3,3;5,5;7,7];
+hydro.plotDofs = [1,1;3,3;5,5];
 writeBEMIOH5(hydro);
 plotBEMIO(hydro);
