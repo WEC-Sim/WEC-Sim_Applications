9872ffee0e1a9597e015fb7f2bdf5de5053843f2
jleonqu <jleonqu@sandia.gov>
2024-12-11 17:14:08 -0700

    Updated bemio.m file

--- OWC/OrificeModel/OrificeModel/hydroData/bemio.m
+++ OWC/OrificeModel/OrificeModel/hydroData/bemio.m
@@ -2,9 +2,9 @@
 warning('off')
 hydro = struct();
 hydro = readWAMIT(hydro,'test17a.out',[]);
-hydro = radiationIRF(hydro,20,[],[],[],11);
+hydro = radiationIRF(hydro,20,[],[],[],[]);
 hydro = radiationIRFSS(hydro,20,[]);
-hydro = excitationIRF(hydro,20,[],[],[],11);
+hydro = excitationIRF(hydro,20,[],[],[],[]);
 hydro.plotDofs = [1,1;3,3;5,5];
 writeBEMIOH5(hydro);
 plotBEMIO(hydro);
