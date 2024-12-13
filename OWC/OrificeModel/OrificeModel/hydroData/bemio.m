c2014669033436447bb68c4ac0f9c5fcc4f1f172
jleonqu <jleonqu@sandia.gov>
2024-12-13 09:45:37 -0700

    Adding files from the original orificeModel example

--- OWC/OrificeModel/OrificeModel/hydroData/bemio.m
+++ OWC/OrificeModel/OrificeModel/hydroData/bemio.m
@@ -1,10 +1,9 @@
 % Triton OWC - WAMIT GBM simulation
-warning('off')
 hydro = struct();
 hydro = readWAMIT(hydro,'test17a.out',[]);
-hydro = radiationIRF(hydro,20,[],[],[],[]);
+hydro = radiationIRF(hydro,20,[],[],[],11);
 hydro = radiationIRFSS(hydro,20,[]);
-hydro = excitationIRF(hydro,20,[],[],[],[]);
-hydro.plotDofs = [1,1;3,3;5,5];
+hydro = excitationIRF(hydro,20,[],[],[],11);
+hydro.plotDofs = [1,1;3,3;5,5;7,7];
 writeBEMIOH5(hydro);
 plotBEMIO(hydro);
