ad73ab315e4939dfca7b042bfc0f03d7eaf5ee10
akeeste <akeeste@sandia.gov>
2025-01-23 15:26:19 -0600

    remove annoying extra spaces

--- Passive_Yaw/TestPassiveYawRegression.m
+++ Passive_Yaw/TestPassiveYawRegression.m
@@ -7,18 +7,18 @@ classdef TestPassiveYawRegression < matlab.unittest.TestCase
         IrrYaw
     end
     
-    methods(Access = 'public')        
-        function obj = TestPassiveYawRegression(openCompare)            
+    methods(Access = 'public')
+        function obj = TestPassiveYawRegression(openCompare)
             arguments
                 openCompare (1,1) double = 1
-            end            
+            end
             % Assign arguments to test Class
-            obj.openCompare = openCompare;            
+            obj.openCompare = openCompare;
             % Set test directory
-            obj.testDir = fileparts(mfilename('fullpath'));            
+            obj.testDir = fileparts(mfilename('fullpath'));
             % Save the visibility state at construction
-            obj.OriginalDefault = get(0,'DefaultFigureVisible');            
-        end        
+            obj.OriginalDefault = get(0,'DefaultFigureVisible');
+        end
     end
     
     methods (TestMethodSetup)
@@ -27,30 +27,30 @@ classdef TestPassiveYawRegression < matlab.unittest.TestCase
         end
     end
     
-    methods(TestClassSetup)                              
-        function runYawIrrTest(testCase)            
+    methods(TestClassSetup)
+        function runYawIrrTest(testCase)
             cd(fullfile(testCase.testDir,   ...
-                        'PassiveYawRegression'))                     
+                        'PassiveYawRegression'))
             runLoadPassiveYawIrr;
             testCase.IrrYaw = load('IrrYaw.mat').("IrrYaw");
-        end        
-    end
-    
-    methods(TestClassTeardown)        
-        function plotRegressionTests(testCase)            
-            % Open new vs. org Comparisons
-            if testCase.openCompare == 1                
-                cd(testCase.testDir);
-                open(fullfile('.','PassiveYawRegression','figYawIrr.fig')); 
-            end            
-            set(0,'DefaultFigureVisible',testCase.OriginalDefault);
-            testCase.assertEqual(get(0,'DefaultFigureVisible'),     ...
-                                 testCase.OriginalDefault);            
-            cd(testCase.testDir);            
         end
     end
     
-    methods(Test)          
+    methods(TestClassTeardown)
+        function plotRegressionTests(testCase)
+            % Open new vs. org Comparisons
+            if testCase.openCompare == 1
+                cd(testCase.testDir);
+                open(fullfile('.','PassiveYawRegression','figYawIrr.fig')); 
+            end
+            set(0,'DefaultFigureVisible',testCase.OriginalDefault);
+            testCase.assertEqual(get(0,'DefaultFigureVisible'),     ...
+                                 testCase.OriginalDefault);
+            cd(testCase.testDir);
+        end
+    end
+    
+    methods(Test)
         function body1_irregYaw_disp_yaw(testCase)
             % Body1 Displacement in Yaw
             tol = 1e-10;
@@ -64,7 +64,7 @@ classdef TestPassiveYawRegression < matlab.unittest.TestCase
             testCase.verifyEqual(testCase.IrrYaw.Force_diff,0,'AbsTol',tol);
             fprintf(['Body1 Torque in Yaw, Diff = '             ...
                      num2str(testCase.IrrYaw.Force_diff) '\n']);
-        end        
+        end
         function irregYaw_0th_Spectral_Moment(testCase)
             % 0th Order Spectral Moment
             tol = 1e-10;
@@ -73,7 +73,7 @@ classdef TestPassiveYawRegression < matlab.unittest.TestCase
             testCase.verifyEqual(new,org,'AbsTol',tol);
             fprintf(['0th Order Spectral Moment, Diff = '       ...
                       num2str(max(abs(org-new))) '\n']);
-        end        
+        end
         function irregYaw_2nd_Spectral_Moment(testCase)
             % 2nd Order Spectral Moment
             tol = 1e-10;
@@ -82,6 +82,6 @@ classdef TestPassiveYawRegression < matlab.unittest.TestCase
             testCase.verifyEqual(new,org,'AbsTol',tol);
             fprintf(['2nd Order Spectral Moment, Diff = '       ...
                      num2str(max(abs(org-new))) '\n']);
-        end        
+        end
     end    
 end
