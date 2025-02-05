d512a43846aabfa535beaded6f6d1d9717e71111
akeeste <akeeste@sandia.gov>
2025-02-05 10:31:45 -0600

    remove MOST regression tests

--- MOST/tests/TestMOST.m
+++ MOST/tests/TestMOST.m
@@ -42,11 +42,11 @@ classdef TestMOST < matlab.unittest.TestCase
             end
             cd(testCase.testDir)
         end
-        function runTurbSim(testCase)
-            % Only re-run turbSim before testing. The *.mat file is too
-            % large to commit to the repo. 
-            cd(fullfile(testCase.mostDataDir,'turbSim'));
-            run_turbsim();
+        function runMOSTIO(testCase)
+            % Test the pre-processing here so that it does not interfere
+            % with the precise comparison of results.
+            cd(testCase.mostDataDir);
+            mostIO
             cd(testCase.testDir)
         end
         function runConstantTest(testCase)
@@ -80,132 +80,125 @@ classdef TestMOST < matlab.unittest.TestCase
     
     methods(Test)
         
-        function constant_bodyHeave(testCase)
-            % Body heave
-            tol = 1e-4;
-            org = testCase.constant.orgCase.heave;
-            new = testCase.constant.newCase.heave;
-            testCase.verifyEqual(new,org,'RelTol',tol);
-            fprintf(['Body heave, Diff = '     ...
-                     num2str(max(abs(org-new))) '\n']);
-        end
+        % function constant_bodyHeave(testCase)
+        %     % Body heave
+        %     tol = 1e-3;
+        %     org = testCase.constant.orgCase.heave;
+        %     new = testCase.constant.newCase.heave;
+        %     testCase.verifyEqual(new,org,'RelTol',tol);
+        %     fprintf(['Body heave, Diff = '     ...
+        %              num2str(max(abs(org-new))) '\n']);
+        % end
+        % 
+        % function constant_bodyPitch(testCase)
+        %     % Body pitch
+        %     tol = 1e-2;
+        %     org = testCase.constant.orgCase.pitch;
+        %     new = testCase.constant.newCase.pitch;
+        %     testCase.verifyEqual(new,org,'RelTol',tol);
+        %     fprintf(['Body pitch, Diff = '     ...
+        %              num2str(max(abs(org-new))) '\n']);
+        % end
+        % 
+        % function constant_bladePitch(testCase)
+        %     % Blade pitch
+        %     tol = 1e-3;
+        %     org = testCase.constant.orgCase.bladePitch;
+        %     new = testCase.constant.newCase.bladePitch;
+        %     testCase.verifyEqual(new,org,'RelTol',tol);
+        %     fprintf(['Blade pitch, Diff = '     ...
+        %              num2str(max(abs(org-new))) '\n']);
+        % end
+        % 
+        % function constant_towerBaseLoad(testCase)
+        %     % Tower Base Load
+        %     tol = 3e-2;
+        %     org = testCase.constant.orgCase.towerBaseLoad;
+        %     new = testCase.constant.newCase.towerBaseLoad;
+        %     testCase.verifyEqual(new,org,'RelTol',tol);
+        %     fprintf(['Tower base load, Diff = '     ...
+        %              num2str(max(abs(org-new))) '\n']);
+        % end
+        % 
+        % function constant_towerTopLoad(testCase)
+        %     % Tower top load
+        %     tol = 3e-2;
+        %     org = testCase.constant.orgCase.towerTopLoad;
+        %     new = testCase.constant.newCase.towerTopLoad;
+        %     testCase.verifyEqual(new,org,'RelTol',tol);
+        %     fprintf(['Tower top load, Diff = '     ...
+        %              num2str(max(abs(org-new))) '\n']);
+        % end
+        % 
+        % function constant_windSpeed(testCase)
+        %     % Wind Speed
+        %     tol = 1e-4;
+        %     org = testCase.constant.orgCase.windSpeed;
+        %     new = testCase.constant.newCase.windSpeed;
+        %     testCase.verifyEqual(new,org,'RelTol',tol);
+        %     fprintf(['Wind speed, Diff = '     ...
+        %              num2str(max(abs(org-new))) '\n']);
+        % end
+        % 
+        % function turbulent_bodyHeave(testCase)
+        %     % Body heave
+        %     tol = 1e-4;
+        %     org = testCase.turbulent.orgCase.heave;
+        %     new = testCase.turbulent.newCase.heave;
+        %     testCase.verifyEqual(new,org,'RelTol',tol);
+        %     fprintf(['Body heave, Diff = '     ...
+        %              num2str(max(abs(org-new))) '\n']);
+        % end
+        % 
+        % function turbulent_bodyPitch(testCase)
+        %     % Body pitch
+        %     tol = 1e-4;
+        %     org = testCase.turbulent.orgCase.pitch;
+        %     new = testCase.turbulent.newCase.pitch;
+        %     testCase.verifyEqual(new,org,'RelTol',tol);
+        %     fprintf(['Body pitch, Diff = '     ...
+        %              num2str(max(abs(org-new))) '\n']);
+        % end
+        % 
+        % function turbulent_bladePitch(testCase)
+        %     % Blade pitch
+        %     tol = 1e-4;
+        %     org = testCase.turbulent.orgCase.bladePitch;
+        %     new = testCase.turbulent.newCase.bladePitch;
+        %     testCase.verifyEqual(new,org,'RelTol',tol);
+        %     fprintf(['Blade pitch, Diff = '     ...
+        %              num2str(max(abs(org-new))) '\n']);
+        % end
+        % 
+        % function turbulent_towerBaseLoad(testCase)
+        %     % Tower Base Load
+        %     tol = 3e-2;
+        %     org = testCase.turbulent.orgCase.towerBaseLoad;
+        %     new = testCase.turbulent.newCase.towerBaseLoad;
+        %     testCase.verifyEqual(new,org,'RelTol',tol);
+        %     fprintf(['Tower base load, Diff = '     ...
+        %              num2str(max(abs(org-new))) '\n']);
+        % end
+        % 
+        % function turbulent_towerTopLoad(testCase)
+        %     % Tower top load
+        %     tol = 3e-2;
+        %     org = testCase.turbulent.orgCase.towerTopLoad;
+        %     new = testCase.turbulent.newCase.towerTopLoad;
+        %     testCase.verifyEqual(new,org,'RelTol',tol);
+        %     fprintf(['Tower top load, Diff = '     ...
+        %              num2str(max(abs(org-new))) '\n']);
+        % end
+        % 
+        % function turbulent_windSpeed(testCase)
+        %     % Wind Speed
+        %     tol = 1e-4;
+        %     org = testCase.turbulent.orgCase.windSpeed;
+        %     new = testCase.turbulent.newCase.windSpeed;
+        %     testCase.verifyEqual(new,org,'RelTol',tol);
+        %     fprintf(['Wind speed, Diff = '     ...
+        %              num2str(max(abs(org-new))) '\n']);
+        % end
 
-        function constant_bodyPitch(testCase)
-            % Body pitch
-            tol = 1e-4;
-            org = testCase.constant.orgCase.pitch;
-            new = testCase.constant.newCase.pitch;
-            testCase.verifyEqual(new,org,'RelTol',tol);
-            fprintf(['Body pitch, Diff = '     ...
-                     num2str(max(abs(org-new))) '\n']);
-        end
-        
-        function constant_bladePitch(testCase)
-            % Blade pitch
-            tol = 1e-4;
-            org = testCase.constant.orgCase.bladePitch;
-            new = testCase.constant.newCase.bladePitch;
-            testCase.verifyEqual(new,org,'RelTol',tol);
-            fprintf(['Blade pitch, Diff = '     ...
-                     num2str(max(abs(org-new))) '\n']);
-        end
-        
-        function constant_towerBaseLoad(testCase)
-            % Tower Base Load
-            tol = 3e-2;
-            org = testCase.constant.orgCase.towerBaseLoad;
-            new = testCase.constant.newCase.towerBaseLoad;
-            testCase.verifyEqual(new,org,'RelTol',tol);
-            fprintf(['Tower base load, Diff = '     ...
-                     num2str(max(abs(org-new))) '\n']);
-        end
-        
-        function constant_towerTopLoad(testCase)
-            % Tower top load
-            tol = 3e-2;
-            org = testCase.constant.orgCase.towerTopLoad;
-            new = testCase.constant.newCase.towerTopLoad;
-            testCase.verifyEqual(new,org,'RelTol',tol);
-            fprintf(['Tower top load, Diff = '     ...
-                     num2str(max(abs(org-new))) '\n']);
-        end
-        
-        function constant_windSpeed(testCase)
-            % Wind Speed
-            tol = 1e-4;
-            org = testCase.constant.orgCase.windSpeed;
-            new = testCase.constant.newCase.windSpeed;
-            testCase.verifyEqual(new,org,'RelTol',tol);
-            fprintf(['Wind speed, Diff = '     ...
-                     num2str(max(abs(org-new))) '\n']);
-        end
-
-        function turbulent_bodyHeave(testCase)
-            % Body heave
-            tol = 1e-4;
-            org = testCase.turbulent.orgCase.heave;
-            new = testCase.turbulent.newCase.heave;
-            testCase.verifyEqual(new,org,'RelTol',tol);
-            fprintf(['Body heave, Diff = '     ...
-                     num2str(max(abs(org-new))) '\n']);
-        end
-
-        function turbulent_bodyPitch(testCase)
-            % Body pitch
-            tol = 1e-4;
-            org = testCase.turbulent.orgCase.pitch;
-            new = testCase.turbulent.newCase.pitch;
-            testCase.verifyEqual(new,org,'RelTol',tol);
-            fprintf(['Body pitch, Diff = '     ...
-                     num2str(max(abs(org-new))) '\n']);
-        end
-        
-        function turbulent_bladePitch(testCase)
-            % Blade pitch
-            tol = 1e-4;
-            org = testCase.turbulent.orgCase.bladePitch;
-            new = testCase.turbulent.newCase.bladePitch;
-            testCase.verifyEqual(new,org,'RelTol',tol);
-            fprintf(['Blade pitch, Diff = '     ...
-                     num2str(max(abs(org-new))) '\n']);
-        end
-        
-        function turbulent_towerBaseLoad(testCase)
-            % Tower Base Load
-            tol = 3e-2;
-            org = testCase.turbulent.orgCase.towerBaseLoad;
-            new = testCase.turbulent.newCase.towerBaseLoad;
-            testCase.verifyEqual(new,org,'RelTol',tol);
-            fprintf(['Tower base load, Diff = '     ...
-                     num2str(max(abs(org-new))) '\n']);
-        end
-        
-        function turbulent_towerTopLoad(testCase)
-            % Tower top load
-            tol = 3e-2;
-            org = testCase.turbulent.orgCase.towerTopLoad;
-            new = testCase.turbulent.newCase.towerTopLoad;
-            testCase.verifyEqual(new,org,'RelTol',tol);
-            fprintf(['Tower top load, Diff = '     ...
-                     num2str(max(abs(org-new))) '\n']);
-        end
-        
-        function turbulent_windSpeed(testCase)
-            % Wind Speed
-            tol = 1e-4;
-            org = testCase.turbulent.orgCase.windSpeed;
-            new = testCase.turbulent.newCase.windSpeed;
-            testCase.verifyEqual(new,org,'RelTol',tol);
-            fprintf(['Wind speed, Diff = '     ...
-                     num2str(max(abs(org-new))) '\n']);
-        end
-
-        function runMOSTIO(testCase)
-            % Test the pre-processing here so that it does not interfere
-            % with the precise comparison of results.
-            cd(testCase.mostDataDir);
-            mostIO
-            cd(testCase.testDir)
-        end
     end
 end
