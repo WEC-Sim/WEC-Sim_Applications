534b6876749b593f626888b1da9973c68e4a89e8
akeeste <akeeste@sandia.gov>
2025-08-04 12:54:30 -0500

    fix BEMIO path

--- MOST/hydroData/VolturnUS/bemio.m
+++ MOST/hydroData/VolturnUS/bemio.m
@@ -1,6 +1,6 @@
 
 hydro = struct();
-hydro = readNEMOH(hydro,fullfile('VolturnUS15MW_nemoh'));
+hydro = readNEMOH(hydro,'.');
 hydro = radiationIRF(hydro,90,201,201,[],[]);
 hydro = radiationIRFSS(hydro,[],[]);
 hydro = excitationIRF(hydro,90,[],[],[],[]);
