9d1ba8d067be2dca0115e6aae004224bad0d9fe2
MShabara <mshabara@nrel.gov>
2025-08-08 21:22:00 -0600

    modified the latching control gain calc

--- Controls/Latching/optimalTimeCalc.m
+++ Controls/Latching/optimalTimeCalc.m
@@ -6,12 +6,16 @@ close all; clear all; clc;
 
 % Inputs (from wecSimInputFile)
 simu = simulationClass();
-body(1) = bodyClass('../hydroData/sphere.h5');
+body(1) = bodyClass('../../_Common_Input_Files/Sphere/hydroData/sphere.h5');
 waves.height = 2.5;
 waves.period = 9.6664;
 
+
 % Load hydrodynamic data for float from BEM
 hydro = readBEMIOH5(body.h5File{1}, 1, body.meanDrift);
+
+dof = 3;            % Caluclate for heave motion
+mass = simu.rho*hydro.properties.volume;
 
 % Define wave conditions
 H = waves.height;
@@ -19,20 +23,30 @@ A = H/2;
 T = waves.period;
 omega = (1/T)*(2*pi);
 
+% Extend the freq vector to include the wave frequency
+hydro.simulation_parameters.w_extended = sort([hydro.simulation_parameters.w omega]);
+omegaIndex = find(hydro.simulation_parameters.w_extended == omega, 1, 'first');
+
 % Define excitation force based on wave conditions
-ampSpect = zeros(length(hydro.simulation_parameters.w),1);
-[~,closestIndOmega] = min(abs(omega-hydro.simulation_parameters.w));
-ampSpect(closestIndOmega) = A;
-FeRao = squeeze(hydro.hydro_coeffs.excitation.re(3,:))'*simu.rho*simu.gravity + squeeze(hydro.hydro_coeffs.excitation.im(3,:))'*simu.rho*simu.gravity*1j;
-Fexc = ampSpect.*FeRao;
+ampSpect = zeros(length(hydro.simulation_parameters.w_extended),1);
+[~,closestIndOmega] = min(abs(omega-hydro.simulation_parameters.w_extended));
+ampSpect(omegaIndex) = A;
+Fe_re = squeeze(hydro.hydro_coeffs.excitation.re(dof, 1, :)) * simu.rho * simu.gravity;
+Fe_im = squeeze(hydro.hydro_coeffs.excitation.im(dof, 1, :)) * simu.rho * simu.gravity;
+
+Fe_interp = interp1(hydro.simulation_parameters.w, Fe_re + 1j * Fe_im, hydro.simulation_parameters.w_extended, 'spline', 'extrap')';
+Fexc = ampSpect.*Fe_interp;
 
 % Define the intrinsic mechanical impedance for the device
-mass = simu.rho*hydro.properties.volume;
-addedMass = squeeze(hydro.hydro_coeffs.added_mass.all(3,3,:))*simu.rho;
-radiationDamping = squeeze(hydro.hydro_coeffs.radiation_damping.all(3,3,:)).*squeeze(hydro.simulation_parameters.w')*simu.rho;
-hydrostaticStiffness = hydro.hydro_coeffs.linear_restoring_stiffness(3,3)*simu.rho*simu.gravity;
-Gi = -((hydro.simulation_parameters.w)'.^2.*(mass+addedMass)) + 1j*hydro.simulation_parameters.w'.*radiationDamping + hydrostaticStiffness;
-Zi = Gi./(1j*hydro.simulation_parameters.w');
+addedMass = squeeze(hydro.hydro_coeffs.added_mass.all(dof, dof, :)) * simu.rho;
+addedMass = interp1(hydro.simulation_parameters.w, addedMass, hydro.simulation_parameters.w_extended, 'spline', 'extrap')';
+
+radiationDamping = squeeze(hydro.hydro_coeffs.radiation_damping.all(dof,dof,:)).*squeeze(hydro.simulation_parameters.w')*simu.rho;
+radiationDamping = interp1(hydro.simulation_parameters.w, radiationDamping, hydro.simulation_parameters.w_extended, 'spline', 'extrap')';
+
+hydrostaticStiffness = hydro.hydro_coeffs.linear_restoring_stiffness(dof, dof) * simu.rho * simu.gravity;
+Gi = -((hydro.simulation_parameters.w_extended)'.^2 .* (mass+addedMass)) + 1j * hydro.simulation_parameters.w_extended'.*radiationDamping + hydrostaticStiffness;
+Zi = Gi./(1j*hydro.simulation_parameters.w_extended');
 
 % Calculate magnitude and phase for bode plot
 Mag = 20*log10(abs(Zi));
@@ -40,13 +54,13 @@ Phase = (angle(Zi))*(180/pi);
 
 % Determine natural frequency based on the phase of the impedance
 [~,closestIndNat] = min(abs(imag(Zi)));
-natFreq = hydro.simulation_parameters.w(closestIndNat);
+natFreq = hydro.simulation_parameters.w_extended(closestIndNat);
 natT = (2*pi)/natFreq;
 
 % Create bode plot for impedance
 figure()
 subplot(2,1,1)
-semilogx((hydro.simulation_parameters.w)/(2*pi),Mag)
+semilogx((hydro.simulation_parameters.w_extended)/(2*pi),Mag)
 xlabel('freq (hz)')
 ylabel('mag (dB)')
 grid on
@@ -55,7 +69,7 @@ xline(1/T,'--')
 legend('','Natural Frequency','Wave Frequency','Location','southwest')
 
 subplot(2,1,2)
-semilogx((hydro.simulation_parameters.w)/(2*pi),Phase)
+semilogx((hydro.simulation_parameters.w_extended)/(2*pi),Phase)
 xlabel('freq (hz)')
 ylabel('phase (deg)')
 grid on
@@ -65,10 +79,8 @@ legend('','Natural Frequency','Wave Frequency','Location','northwest')
 
 % Determine optimal latching time
 optLatchTime = 0.5*(T - natT)
-KpOpt = radiationDamping(closestIndOmega)
-force = 80*(mass+addedMass(closestIndOmega))
+KpOpt = radiationDamping(omegaIndex)
+force = 80*(mass+addedMass(omegaIndex))
 
 % Calculate the maximum potential power
 P_max = -sum(abs(Fexc).^2./(8*real(Zi)))
-
-
