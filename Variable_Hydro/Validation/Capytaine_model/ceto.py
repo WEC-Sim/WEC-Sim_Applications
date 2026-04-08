b8b5221e25e973b645b6b9f53ddc92c0d313a3fc
akeeste <akeeste@sandia.gov>
2026-04-08 06:39:05 -0600

    variable hydro validation folder and ceto BEM mesh resolution results

---
+++ Variable_Hydro/Validation/Capytaine_model/ceto.py
@@ -0,0 +1,91 @@
+import numpy as np
+import xarray as xr
+import capytaine as cpt
+from capytaine.io.legacy import export_hydrostatics
+import os as os
+
+
+def ceto(depth, resolution, output_dir, output_file):
+    """
+    Capytaine simulations of the Carnegie Energy CETO device.
+
+    This function takes in cylinder depth and output directory, creates a
+    mesh using Capytaine, and runs Capytaine. Use iterative calls to
+    this function to conduct a mesh resolution study, iterate over geometry
+    parameters, or run a single case.
+
+    Saves the Capytaine output and hydrostatic information to the specified
+    directory
+
+    Parameters
+    ----------
+    depth: float
+        depth of the cylinder center (positive up) [m]
+    resolution: 3x1 tuple
+        Number of panels in the cylinder nr, ntheta, nz directions [-,-,-]
+    output_dir: string
+        Path to the directory where the results and hydrostatic information are saved
+    output_file: string
+        Name of the output file in directory where the results and hydrostatic information are saved
+
+    Returns
+    -------
+    dataset: xarray.Dataset
+        Capytaine output
+
+    """
+    cg = (0, 0, depth)
+
+    mesh = cpt.mesh_vertical_cylinder(
+        length=5,
+        radius=12.5,
+        center=cg,
+        resolution=resolution,
+    )
+    body = cpt.FloatingBody(
+        mesh=mesh,
+        dofs=cpt.rigid_body_dofs(rotation_center=cg),
+        center_of_mass=cg,
+        name="ceto",
+    )
+    body.inertia_matrix = body.compute_rigid_body_inertia()
+    body.hydrostatic_stiffness = body.immersed_part().compute_hydrostatic_stiffness()
+
+    # body.show()  # Uncomment to display the mesh in 3D for verification
+
+    test_matrix = xr.Dataset(
+        coords={
+            "omega": np.linspace(0.5, 20.0, 40),
+            "radiating_dof": list(body.dofs),
+            "wave_direction": [0],
+            "water_depth": [30.0],
+            "rho": [1000.0],
+        }
+    )
+
+    solver = cpt.BEMSolver()
+    dataset = solver.fill_dataset(test_matrix, body.immersed_part(), n_jobs=4)
+
+    # add extras to the dataset
+    dataset["center_of_mass"] = (
+        ["rigid_body_component", "point_coordinates"],
+        [body.center_of_mass],
+    )
+    dataset["center_of_buoyancy"] = (
+        ["rigid_body_component", "point_coordinates"],
+        [body.center_of_buoyancy],
+    )
+    dataset["volume"] = (
+        ["rigid_body_component"],
+        [body.volume],
+    )
+
+    # Save dataset to .nc
+    dataset["radiating_dof"] = dataset["radiating_dof"].astype(str)
+    dataset["influenced_dof"] = dataset["influenced_dof"].astype(str)
+    # cpt.io.xarray.separate_complex_values(dataset).to_netcdf(
+    #     os.path.join(output_dir, output_file)
+    # )
+
+    cpt.export_dataset(os.path.join(output_dir, output_file), dataset)
+    # export_hydrostatics(output_dir, body)
---
+++ Variable_Hydro/Validation/Capytaine_model/mesh_resolution.py
@@ -0,0 +1,26 @@
+import os as os
+import ceto as ceto
+import xarray as xr
+
+input_data_dir = os.path.dirname(".")
+
+depths = [-9]  # nominal depth about which the CETO will oscillate
+dataset = [xr.Dataset(), xr.Dataset(), xr.Dataset()]
+for depth in depths:
+    for i, resolution in enumerate(
+        # [(5, 16, 3), (10, 32, 5), (20, 64, 10), (40, 128, 20)]
+        [(40, 128, 20)]
+    ):
+        output_dir = os.path.join(input_data_dir, "mesh_resolution")
+        os.makedirs(output_dir, exist_ok=True)
+
+        output_file = (
+            str(resolution[0])
+            + "_"
+            + str(resolution[1])
+            + "_"
+            + str(resolution[2])
+            + "_output.nc"
+        )
+        print("Running ", resolution)
+        dataset[i] = ceto.ceto(depth, resolution, output_dir, output_file)
