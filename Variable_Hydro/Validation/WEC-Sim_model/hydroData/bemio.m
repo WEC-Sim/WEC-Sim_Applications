d5ffbb63f2d816dc979aa0971a1512b72091b156
akeeste <akeeste@sandia.gov>
2026-04-09 13:42:09 -0600

    wec-sim model directory and bemio.m

---
+++ Variable_Hydro/Validation/WEC-Sim_model/hydroData/bemio.m
@@ -0,0 +1,24 @@
+% Script to read in BEM mesh resolution data and visualize
+
+% Read all .nc files in this directory.
+% Convert .nc filenames to resolution label and numbers
+d = dir("*.nc");
+depth_str = erase(string({d(:).name}), [".nc", "depth_"]);
+
+depths = cellfun(@str2num, depth_str);
+[depths, i_sort] = sort(depths);
+depth_str = depth_str(i_sort);
+
+% Read all .nc files
+hydroData = cell(1, length(depths));
+for i = 1:length(depth_str)
+    name = "depth_" + depth_str(i) + ".nc";
+    file = fullfile("..", "Capytaine_model", "depth_variation", filename);
+    hydro = readCAPYTAINE(struct(), file);
+    hydro = radiationIRF(hydro, 60, [], [], [], []);
+    hydro = excitationIRF(hydro, 60, [], [], [], []);
+    hydroData{i} = hydro;
+end
+
+%%
+
